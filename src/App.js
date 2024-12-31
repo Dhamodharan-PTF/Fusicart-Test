9b30a6682db571d8d25ff8907a962ea414a0b7a0
unknown <dhamodharan.infoteamptf@gmail.com>
2024-12-31 15:05:38 +0530

    Old Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Index from './/fuscart/index.tsx'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Index />
+      </header>
+    </div>
+  );
+}
+
+export default App;
